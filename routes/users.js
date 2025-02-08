807dff42ae5ad3fe5e9c1449b6f47ff4e77bb800
Alevinmol <vinay2@byui.edu>
2025-02-08 17:06:16 -0600

    upsateed routes and rest

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const contactsController = require('../controllers/users');
 
 router.get('/', contactsController.getAll);
+
 router.post('/', contactsController.createUser);
+
+router.put('/:id');
+
+router.delete('/:id');
 
 module.exports = router;
